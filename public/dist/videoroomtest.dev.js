525dbe63f4b9689c8e892aaad572f2a5ffccf88c
sesaretah <hs.shafiei@gmail.com>
2021-07-14 23:44:41 +0430

    1

--- public/dist/videoroomtest.dev.js
+++ public/dist/videoroomtest.dev.js
@@ -659,8 +659,8 @@ function streamAttacher(feed, display) {
   }
 
   if (feed.id && feed.webrtcStuff && feed.webrtcStuff.remoteStream) {
-    $("#v" + roomId[1]).html();
-    $("#v" + roomId[1]).append(localVideo);
+    $("#v" + roomId[1]).empty();
+    $("#v" + roomId[1]).prepend(localVideo);
     Janus.attachMediaStream(document.getElementById("video-" + feed.id), feed.webrtcStuff.remoteStream);
   }
 
